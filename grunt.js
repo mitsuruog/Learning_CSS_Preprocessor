39fe041d03d29182d856d0b685942bceb5a72af7
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2013-01-17 12:18:26 -0500

    [WIP]tests Operator

---
+++ grunt.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        stylus: {
+            compile: {
+                options: {
+                    compress: false,
+                    //      paths: ['path/to/import', 'another/to/import'],
+                    //      urlfunc: 'embedurl', // use embedurl('test.png') in our code to trigger Data URI embedding
+                    //      use: [
+                    //        require('fluidity') // use stylus plugin at compile time
+                    //      ]
+                },
+                files: {
+                    'stylus/operator.css': 'stylus/src/operator.styl', // 1:1 compile
+                    //      'main.css': ['src/*.styl', 'path/to/more/*.styl'] // compile and concat into single file
+                }
+            }
+        },
+        watch: {
+            files: ['stylus/src/*.styl'],
+            tasks: ['stylus']
+        }
+    });
+
+    // Load npm tasks
+    //grunt.loadNpmTasks('grunt-csso');
+    //grunt.loadNpmTasks('grunt-less');
+
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+    
+    // Default task.
+    grunt.registerTask('default', 'watch');
+
+};
